07afff07d2dc77f98da5e5083989b9d92c33d9a1
emidret <emidretcanu@u.boisestate.edu>
2024-10-12 09:33:02 -0600

    10/12/24

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+var p = document.querySelector('p');
+var count = 0;
+
 // function to generate random number
 
 function random(min, max) {
@@ -36,7 +39,7 @@ class Ball extends Shape {
     //this.velY = velY;
     this.color = color;
     this.size = size;
-    this.exists = true;
+    this.exists = new Boolean(true);
   }
 
   draw() {
@@ -74,9 +77,85 @@ class Ball extends Shape {
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
 
-        if(this.exists) {
+        if(this.exists.valueOf()) {
           if (distance < this.size + ball.size) {
-            ball.color = this.color = randomRGB();
+            //ball.color = this.color = randomRGB();
+            this.color = randomRGB();
+          }
+        }
+      }
+    }
+  }
+}
+
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch(e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    })
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists.valueOf()) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.x = 0;
+          ball.y = 0;
+          ball.velX = 0;
+          ball.velY = 0;
+          ball.color = 'blue';
+          ball.size = 0;
+          if(ball.exists.valueOf()) {
+            ball.exists = new Boolean(false);
+            count++;
+            p.textContent = "Ball Count: " + count;
           }
         }
       }
@@ -102,6 +181,8 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilcircle = new EvilCircle(random(0, width), random(0, height));
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
@@ -111,6 +192,10 @@ function loop() {
     ball.update();
     ball.collisionDetect();
   }
+
+  evilcircle.draw();
+  evilcircle.checkBounds();
+  evilcircle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
